93468a25aced50339fae93d0eee55b51d54b8546
renusha <renushaashen@gmail.com>
2026-07-08 12:59:05 +0530

    created UI

--- weatherapp.py
+++ weatherapp.py
@@ -2,3 +2,17 @@ import sys
 import requests
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton
 from PyQt5.QtCore import Qt
+
+class WeatherApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.city_label = QLabel("Enter city name : ",self)
+        self.city_input = QLineEdit()
+        self.get_button = QPushButton("Get Weather")
+        
+if __name__ == "__main__":      
+    app = QApplication(sys.argv)
+    weather_app = WeatherApp()
+    weather_app.show()
+    sys.exit(app.exec_())         
+        
